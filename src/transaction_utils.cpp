d529a596b77c544a41b468266388daadf11e375c
Jeremy Kauffman <jeremy@blockstream.io>
2019-03-21 17:56:07 +0000

    Remove redundant check on send amount
    Maximum value of satoshi is now being checked in convert_amount

--- src/transaction_utils.cpp
+++ src/transaction_utils.cpp
@@ -259,11 +259,9 @@ namespace sdk {
             result["error"] = res::id_invalid_amount;
         }
 
-        // Invalid amount if above coin supply.
         // Transactions with outputs below the dust threshold (except OP_RETURN)
         // are not relayed by network nodes
-        if ((satoshi.value() > static_cast<uint64_t>(WALLY_SATOSHI_PER_BTC) * WALLY_BTC_MAX)
-            || (!result.value("send_all", false) && satoshi.value() < session.get_dust_threshold())) {
+        if (!result.value("send_all", false) && satoshi.value() < session.get_dust_threshold()) {
             result["error"] = res::id_invalid_amount;
         }
 
