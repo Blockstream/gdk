f44d880c7522c88b9a657d7ad8f72404a1d5ea91
Lawrence Nahum <lawrence@greenaddress.it>
2019-07-23 11:57:21 +0200

    update to latest fedora 30

--- tools/fedora30_deps.sh
+++ tools/fedora30_deps.sh
@@ -2,7 +2,7 @@
 set -e
 
 dnf update -yq
-dnf install -yq @development-tools wget autoconf pkg-config libtool ninja-build clang which python2 libatomic python3-{devel,virtualenv,wheel} swig
+dnf install -yq @development-tools wget autoconf pkg-config libtool ninja-build clang which python2 libatomic
 pip3 install --require-hashes -r /requirements.txt
 
 dnf clean all
