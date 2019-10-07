3d847fb8f28d8f0189af8927361fbaceb2949ade
Lawrence Nahum <lawrence@greenaddress.it>
2019-10-07 09:58:51 +0200

    update debian buster, add rust windows x-compile support, bsdutils

--- tools/fedora30_deps.sh
+++ tools/fedora30_deps.sh
@@ -5,7 +5,7 @@ dnf update -yq
 dnf install -yq @development-tools wget autoconf pkg-config libtool ninja-build clang which python2 libatomic curl
 pip3 install --require-hashes -r /requirements.txt
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.37.0
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.38.0
 source /root/.cargo/env
 rustup component add rustfmt clippy
 
