b60bcf404657ea859a527fbaca5020715419f0c2
William Casarin <jb55@jb55.com>
2020-01-31 10:01:25 -0800

    gdk_common: take values by reference in Session trait
    Signed-off-by: William Casarin <jb55@jb55.com>

--- subprojects/gdk_rust/gdk_common/src/lib.rs
+++ subprojects/gdk_rust/gdk_common/src/lib.rs
@@ -29,28 +29,29 @@ impl GDKRUST_json {
 }
 
 pub trait Session<E: std::fmt::Debug>: std::marker::Sized {
-    fn create_session(network: Network) -> Result<Self, E>;
-    fn destroy_session(self) -> Result<(), E>;
-    fn poll_session(&self) -> Result<(), E>;
-    fn connect(&mut self, net_params: Value, log_level: u32) -> Result<(), E>;
-    fn disconnect(&mut self) -> Result<(), E>;
-    fn register_user(&mut self, mnemonic: String) -> Result<(), E>;
-    fn login(&mut self, mnemonic: String, password: Option<String>) -> Result<(), E>;
-    fn get_subaccounts(&self) -> Result<Vec<Value>, E>;
-    fn get_subaccount(&self, index: u32) -> Result<Value, E>;
-    fn get_transactions(&self, details: Value) -> Result<Value, E>;
-    fn get_transaction_details(&self, txid: String) -> Result<Value, E>;
-    fn get_balance(&self, details: Value) -> Result<Value, E>;
-    fn set_transaction_memo(&self, txid: String, memo: String, memo_type: u32) -> Result<(), E>;
-    fn create_transaction(&self, details: Value) -> Result<String, E>;
-    fn sign_transaction(&mut self, tx_detail_unsigned: Value) -> Result<Value, E>;
-    fn send_transaction(&self, tx_detail_signed: Value) -> Result<String, E>;
-    fn broadcast_transaction(&self, tx_hex: String) -> Result<String, E>;
-    fn get_receive_address(&self, addr_details: Value) -> Result<Value, E>;
-    fn get_mnemonic_passphrase(&self, _password: String) -> Result<String, E>;
-    fn get_available_currencies(&self) -> Result<Value, E>;
-    fn convert_amount(&self, value_details: Value) -> Result<Value, E>;
-    fn get_fee_estimates(&self) -> Result<Value, E>;
-    fn get_settings(&self) -> Result<Value, E>;
-    fn change_settings(&mut self, settings: Value) -> Result<(), E>;
+
+    fn create_session(network: Network) -> Result<Self, Self::Error>;
+    fn destroy_session(self) -> Result<(), Self::Error>;
+    fn poll_session(&self) -> Result<(), Self::Error>;
+    fn connect(&mut self, net_params: &Value) -> Result<(), Self::Error>;
+    fn disconnect(&mut self) -> Result<(), Self::Error>;
+    // fn register_user(&mut self, mnemonic: String) -> Result<(), Self::Error>;
+    fn login(&mut self, mnemonic: String, password: Option<String>) -> Result<(), Self::Error>;
+    fn get_subaccounts(&self) -> Result<Vec<Value>, Self::Error>;
+    fn get_subaccount(&self, index: u32) -> Result<Value, Self::Error>;
+    fn get_transactions(&self, details: &Value) -> Result<Value, Self::Error>;
+    fn get_transaction_details(&self, txid: String) -> Result<Value, Self::Error>;
+    fn get_balance(&self, details: &Value) -> Result<Value, Self::Error>;
+    fn set_transaction_memo(&self, txid: String, memo: String, memo_type: u32) -> Result<(), Self::Error>;
+    fn create_transaction(&self, details: &Value) -> Result<String, Self::Error>;
+    fn sign_transaction(&self, tx_detail_unsigned: &Value) -> Result<Value, Self::Error>;
+    fn send_transaction(&self, tx_detail_signed: &Value) -> Result<String, Self::Error>;
+    fn broadcast_transaction(&self, tx_hex: String) -> Result<String, Self::Error>;
+    fn get_receive_address(&self, addr_details: &Value) -> Result<Value, Self::Error>;
+    fn get_mnemonic_passphrase(&self, _password: String) -> Result<String, Self::Error>;
+    // fn get_available_currencies(&self) -> Result<Value, Self::Error>;
+    // fn convert_amount(&self, value_details: &Value) -> Result<Value, Self::Error>;
+    fn get_fee_estimates(&self) -> Result<Value, Self::Error>;
+    fn get_settings(&self) -> Result<Value, Self::Error>;
+    fn change_settings(&mut self, settings: &Value) -> Result<(), Self::Error>;
 }
