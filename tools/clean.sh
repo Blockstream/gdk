67f50e143285f38c7e4a7fdb2ae8b8e1952195b4
Jeremy Kauffman <jeremy@blockstream.io>
2020-04-01 12:54:26 +0100

    By default retain package tarballs on clean
    The hashes of the tarballs in subprojects/packagecache are checked
    during the build anyway. Provide option of `tools/clean.sh all` for
    previous default behaviour.

--- tools/clean.sh
+++ tools/clean.sh
@@ -10,10 +10,17 @@ if [ "$1" = "meson" ]; then
 elif [ "$1" = "dirs" ]; then
   find subprojects/ -mindepth 1 -maxdepth 1 -not -path '*-meson*' -not -name '*wrap*' -not -name '*packagecache*' | xargs rm -rf --
   find subprojects/packagecache/ -name '*meson*.tar' | xargs rm -rf --
-else
+elif [ "$1" = "all" ]; then
+  # Clean everything, including the downloaded package tars
   rm -fr build-*
   clean_meson
   find subprojects/ -maxdepth 1 -mindepth 1 -not -name '*meson*' -not -name '*wrap*' | xargs rm -rf --
+else
+  # By default clean everything except the downloaded package tars
+  rm -fr build-*
+  clean_meson
+  find subprojects/ -mindepth 1 -maxdepth 1 -not -path '*-meson*' -not -name '*wrap*' -not -name '*packagecache*' | xargs rm -rf --
+  find subprojects/packagecache/ -name '*meson*.tar' | xargs rm -rf --
 fi
 
 rm -rf docs/build docs/source/session.rst
