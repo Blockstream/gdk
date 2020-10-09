ead888a1fea7fbb103feb7d7558c287f3b4b754e
Jon Griffiths <jon_p_griffiths@yahoo.com>
2020-10-09 20:28:35 +1300

    provide empty target for http fetches of root urls

--- src/utils.cpp
+++ src/utils.cpp
@@ -387,10 +387,12 @@ namespace sdk {
         std::vector<std::string> endpoint_parts;
         algo::split(endpoint_parts, endpoint, algo::is_any_of("/"));
         GDK_RUNTIME_ASSERT(!endpoint_parts.empty());
+        std::string target;
         if (endpoint_parts.size() > 1) {
-            retval["target"] = "/"
+            target = "/"
                 + algo::join(std::vector<std::string>(std::begin(endpoint_parts) + 1, std::end(endpoint_parts)), "/");
         }
+        retval["target"] = target;
         std::vector<std::string> host_parts;
         algo::split(host_parts, endpoint_parts[0], algo::is_any_of(":"));
         GDK_RUNTIME_ASSERT(!host_parts.empty());
