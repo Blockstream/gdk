8ef648cb03aee5f8f92a416bd1195d489d98780b
Jon Griffiths <jon_p_griffiths@yahoo.com>
2020-11-13 16:20:20 +1300

    roll ENABLE_DEBUG and prefix into CONFIGURE_ARGS when building wally
    This fixes missing debug build flag for ios/iphone. Also clean up the
    script generally and remove old workaround for android builds.

--- tools/buildlibwally-core.sh
+++ tools/buildlibwally-core.sh
@@ -21,76 +21,67 @@ if [ ! -d "${MESON_BUILD_ROOT}/libwally-core" ]; then
     cp -r "${MESON_SOURCE_ROOT}/subprojects/${WALLYCORE_NAME}" "${MESON_BUILD_ROOT}/libwally-core"
 fi
 
-ENABLE_SWIG_JAVA=disable-swig-java
-if [ "x$JAVA_HOME" != "x" ]; then
-    ENABLE_SWIG_JAVA=enable-swig-java
-fi
-
 cd "${MESON_BUILD_ROOT}/libwally-core"
 ./tools/cleanup.sh
 ./tools/autogen.sh
 
-$SED -i 's/\"wallycore\"/\"greenaddress\"/' ${MESON_BUILD_ROOT}/libwally-core/src/swig_java/swig.i
+${SED} -i 's/\"wallycore\"/\"greenaddress\"/' ${MESON_BUILD_ROOT}/libwally-core/src/swig_java/swig.i
 
-ENABLE_DEBUG=""
-if [[ $BUILDTYPE = "debug" ]]; then
-    ENABLE_DEBUG="--enable-debug"
+CONFIGURE_ARGS="--enable-static --disable-shared --enable-elements"
+CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-ecmult-static-precomputation"
+CONFIGURE_ARGS="${CONFIGURE_ARGS} --prefix=${MESON_BUILD_ROOT}/libwally-core/build"
+
+if [ "${BUILDTYPE}" = "debug" ]; then
+    CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-debug"
 fi
 
-CONFIGURE_ARGS="--enable-static --disable-shared --enable-elements --enable-ecmult-static-precomputation"
-
-if [ $LTO = "true" ]; then
+if [ "{$LTO}" = "true" ]; then
     EXTRA_FLAGS="-flto"
 fi
 
 if [ "$2" = "--asan" ]; then
-    EXTRA_FLAGS="$EXTRA_FLAGS -fsanitize=address"
+    EXTRA_FLAGS="${EXTRA_FLAGS} -fsanitize=address"
 fi
 
-if [ \( "$1" = "--ndk" \) ]; then
+if [ "$1" = "--ndk" ]; then
     . ${MESON_SOURCE_ROOT}/tools/env.sh
     . tools/android_helpers.sh
-    export CFLAGS="$CFLAGS -DPIC -fPIC $EXTRA_FLAGS"
-    export LDFLAGS="$LDFLAGS $EXTRA_FLAGS"
-    # FIXME: this function can be removed when wally gets updated
-    function android_get_ldflags() {
-       echo $LDFLAGS
-    }
+    export CFLAGS="${CFLAGS} -DPIC -fPIC ${EXTRA_FLAGS}"
+    export LDFLAGS="${LDFLAGS} ${EXTRA_FLAGS}"
 
-    case $HOST_ARCH in
+    case ${HOST_ARCH} in
         x86) HOST_ARCH=i686;;
     esac
 
-    android_build_wally $HOST_ARCH $NDK_TOOLSDIR $ANDROID_VERSION --build=$HOST_OS \
-          $CONFIGURE_ARGS ac_cv_c_bigendian=no --enable-swig-java --disable-swig-python --target=$SDK_PLATFORM $ENABLE_DEBUG --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
-
-    make -o configure install -j$NUM_JOBS
+    android_build_wally ${HOST_ARCH} ${NDK_TOOLSDIR} ${ANDROID_VERSION} --build=${HOST_OS} \
+          ${CONFIGURE_ARGS} ac_cv_c_bigendian=no --enable-swig-java --disable-swig-python --target=${SDK_PLATFORM}
 elif [ \( "$1" = "--iphone" \) -o \( "$1" = "--iphonesim" \) ]; then
     . ${MESON_SOURCE_ROOT}/tools/ios_env.sh $1
 
-    export CFLAGS="$SDK_CFLAGS -isysroot ${IOS_SDK_PATH} -miphoneos-version-min=11.0 -O3 $EXTRA_FLAGS"
-    export LDFLAGS="$SDK_LDFLAGS -isysroot ${IOS_SDK_PATH} -miphoneos-version-min=11.0 $EXTRA_FLAGS"
+    export CFLAGS="${SDK_CFLAGS} -isysroot ${IOS_SDK_PATH} -miphoneos-version-min=11.0 -O3 ${EXTRA_FLAGS}"
+    export LDFLAGS="${SDK_LDFLAGS} -isysroot ${IOS_SDK_PATH} -miphoneos-version-min=11.0 ${EXTRA_FLAGS}"
     export CC=${XCODE_DEFAULT_PATH}/clang
     export CXX=${XCODE_DEFAULT_PATH}/clang++
-    ./configure --host=arm-apple-darwin --with-sysroot=${IOS_SDK_PATH} --build=$HOST_OS \
-                --disable-swig-java --disable-swig-python \
-                $CONFIGURE_ARGS --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
-    make -o configure clean -j$NUM_JOBS
-    make -o configure -j$NUM_JOBS
-    make -o configure install
-elif [ \( "$1" = "--windows" \) ]; then
+    ./configure --host=arm-apple-darwin --with-sysroot=${IOS_SDK_PATH} --build=${HOST_OS} \
+                --disable-swig-java --disable-swig-python ${CONFIGURE_ARGS}
+
+    make -o configure -j${NUM_JOBS}
+elif [ "$1" = "--windows" ]; then
      export CC=x86_64-w64-mingw32-gcc-posix
      export CXX=x86_64-w64-mingw32-g++-posix
-    ./configure --disable-swig-java --disable-swig-python --host=x86_64-w64-mingw32 --build=$HOST_OS $CONFIGURE_ARGS $ENABLE_DEBUG --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
 
-    make -j$NUM_JOBS
-    make install
+    ./configure --disable-swig-java --disable-swig-python --host=x86_64-w64-mingw32 --build=${HOST_OS} ${CONFIGURE_ARGS}
+    make -j${NUM_JOBS}
 else
-    export CFLAGS="$SDK_CFLAGS -DPIC -fPIC $EXTRA_FLAGS"
-    export LDFLAGS="$SDK_LDFLAGS $EXTRA_FLAGS"
+    export CFLAGS="${SDK_CFLAGS} -DPIC -fPIC ${EXTRA_FLAGS}"
+    export LDFLAGS="${SDK_LDFLAGS} ${EXTRA_FLAGS}"
 
-    ./configure --$ENABLE_SWIG_JAVA --host=$HOST_OS $CONFIGURE_ARGS $ENABLE_DEBUG --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
+    ENABLE_SWIG_JAVA=""
+    if [ -n "${JAVA_HOME}" ]; then
+        ENABLE_SWIG_JAVA="--enable-swig-java"
+    fi
 
-    make -j$NUM_JOBS
-    make install
+    ./configure ${ENABLE_SWIG_JAVA} --host=${HOST_OS} ${CONFIGURE_ARGS}
+    make -j${NUM_JOBS}
 fi
+make -o configure install -j${NUM_JOBS}
