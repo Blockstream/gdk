48dfedf4488f1f5790a2a8bb6cf76bbc259f70bf
Jon Griffiths <jon_p_griffiths@yahoo.com>
2021-01-11 08:46:24 +1300

    Add json_add_non_default: set json data only when its value is not the default

--- src/containers.hpp
+++ src/containers.hpp
@@ -23,6 +23,28 @@ namespace sdk {
         return *p;
     }
 
+    // Set a value to a JSON object if it is non-default, otherwise remove any existing value.
+    // This saves space storing the value if a default value is returned when its fetched.
+    template <typename T = std::string>
+    void json_add_non_default(
+        nlohmann::json& data, const std::string& key, const T& value, const T& default_value = T())
+    {
+        const bool is_default = value == default_value;
+        const auto p = data.find(key);
+        const bool found = p != data.end();
+        if (is_default) {
+            if (found) {
+                data.erase(p); // Remove existing value
+            }
+        } else {
+            if (found) {
+                *p = value; // Overwrite existing value
+            } else {
+                data[key] = value; // Insert new value
+            }
+        }
+    }
+
     // Get a value if present and not null, otherwise return a default value
     template <typename T = std::string>
     T json_get_value(const nlohmann::json& data, const std::string& key, const T& default_value = T())
