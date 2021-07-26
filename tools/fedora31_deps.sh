56f2fda222099423469585cd141705da42bc5251
Leonardo Comandini <leo@leocomandini.org>
2021-07-26 14:02:34 +0200

    rust: update MSRV to 1.49.0

--- tools/fedora31_deps.sh
+++ tools/fedora31_deps.sh
@@ -5,7 +5,7 @@ dnf update -yq
 dnf install -yq @development-tools wget autoconf pkg-config libtool ninja-build clang which python python3-pip libatomic curl perl-FindBin
 pip install --require-hashes -r /requirements.txt
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.42.0
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.49.0
 source /root/.cargo/env
 rustup component add rustfmt clippy
 
