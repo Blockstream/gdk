0f3299560239d9bb3de0d8665de7003793b3c593
Riccardo Casatta <riccardo@casatta.it>
2022-02-02 16:20:13 +0100

    rust: expose methods for auth handler login
    specifically: load_store, {get,set}_master_blinding_key, start_threads

--- subprojects/gdk_rust/src/lib.rs
+++ subprojects/gdk_rust/src/lib.rs
@@ -441,6 +441,18 @@ fn handle_session_call(
             .get_unspent_outputs(&serde_json::from_value(input.clone())?)
             .map(|v| json!(v))
             .map_err(Into::into),
+        "load_store" => session
+            .load_store(&serde_json::from_value(input.clone())?)
+            .map(|v| json!(v))
+            .map_err(Into::into),
+        "get_master_blinding_key" => {
+            session.get_master_blinding_key().map_err(Into::into).map(|s| json!(s))
+        }
+        "set_master_blinding_key" => session
+            .set_master_blinding_key(&serde_json::from_value(input.clone())?)
+            .map(|v| json!(v))
+            .map_err(Into::into),
+        "start_threads" => session.start_threads().map_err(Into::into).map(|s| json!(s)),
 
         // "auth_handler_get_status" => Ok(auth_handler.to_json()),
         _ => Err(Error::MethodNotFound {
