237aa67e837a10947e4fce77e04a12c6e131a970
Jon Griffiths <jon_p_griffiths@yahoo.com>
2022-02-24 09:58:42 +1300

    ga_rust: remove unneeded interface code from the rust ffi header

--- subprojects/gdk_rust/gdk_rust.h
+++ subprojects/gdk_rust/gdk_rust.h
@@ -1,29 +1,10 @@
-#ifndef GDK_GDK_RPC_H
-#define GDK_GDK_RPC_H
+#ifndef GDK_GDK_RUST_H
+#define GDK_GDK_RUST_H
 #pragma once
-
-#include <stddef.h>
-#include <stdint.h>
-#include <sys/types.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#if defined(_WIN32)
-#ifdef GDK_BUILD
-#define GDK_API __declspec(dllexport)
-#else
-#define GDK_API
-#endif
-#elif defined(__GNUC__) && defined(GDK_BUILD)
-#define GDK_API __attribute__((visibility("default")))
-#else
-#define GDK_API
-#endif
-
-/** A server session */
-typedef void* GDKRUST_session;
 
 /** A notification handler */
 typedef void (*GDKRUST_notification_handler)(void *self_context, char *details);
@@ -34,9 +15,9 @@ typedef void (*GDKRUST_notification_handler)(void *self_context, char *details);
  * :param session: Destination for the resulting session.
  *|     Returned session should be freed using `GDKRUST_destroy_session`.
  */
-GDK_API int GDKRUST_create_session(GDKRUST_session* session, const char* network);
+int GDKRUST_create_session(void* session, const char* network);
 
-GDK_API int GDKRUST_call_session(GDKRUST_session session, const char *method, const char *input, char** output);
+int GDKRUST_call_session(void* session, const char *method, const char *input, char** output);
 
 /**
  * A collection of stateless functions
@@ -45,33 +26,29 @@ GDK_API int GDKRUST_call_session(GDKRUST_session session, const char *method, co
  * :param input: The json input to pass to the function.
  * :param output: The json output, should be freed using `GDKRUST_destroy_string`.
  */
-GDK_API int GDKRUST_call(const char *method, const char *input, char** output);
+int GDKRUST_call(const char *method, const char *input, char** output);
 
-#ifndef SWIG
 /**
  * Set a handler to be called when notifications arrive.
  */
-GDK_API int GDKRUST_set_notification_handler(GDKRUST_session session, GDKRUST_notification_handler handler, void *self_context);
+int GDKRUST_set_notification_handler(void* session, GDKRUST_notification_handler handler, void *self_context);
 
 /**
  * Free a string returned by the api.
  *
  * :param str: The string to free.
  */
-GDK_API void GDKRUST_destroy_string(char* str);
+void GDKRUST_destroy_string(char* str);
 
 /**
  * Free a session created by the api.
  *
  * :param session: The session to free.
  */
-GDK_API void GDKRUST_destroy_session(GDKRUST_session session);
-
-#endif /* SWIG */
-
+void GDKRUST_destroy_session(void* session);
 
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
 
-#endif /* GDK_GDK_RPC_H */
+#endif /* GDK_GDK_RUST_H */
