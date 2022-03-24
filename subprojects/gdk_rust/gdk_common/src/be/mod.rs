b58d848349ce382b76dbd8da6cb132b70b805f73
Leonardo Comandini <leo@leocomandini.org>
2022-03-24 18:56:48 +0100

    rust: remove unused UTXOInfo

--- subprojects/gdk_rust/gdk_common/src/be/mod.rs
+++ subprojects/gdk_rust/gdk_common/src/be/mod.rs
@@ -7,7 +7,6 @@ mod transaction;
 mod txid;
 
 pub use address::*;
-use bitcoin::hashes::hex::ToHex;
 use bitcoin::util::bip32::DerivationPath;
 pub use blockhash::*;
 pub use blockheader::*;
@@ -16,47 +15,6 @@ pub use script::*;
 use std::fmt::Debug;
 pub use transaction::*;
 pub use txid::*;
-
-#[derive(Debug)]
-pub struct UTXOInfo {
-    pub asset: String,
-    pub value: u64,
-    pub script: BEScript,
-    pub confidential: bool,
-}
-
-impl UTXOInfo {
-    pub fn new_bitcoin(value: u64, script: BEScript) -> Self {
-        UTXOInfo {
-            asset: "btc".to_string(),
-            value,
-            script,
-            confidential: false,
-        }
-    }
-
-    pub fn new_elements(
-        asset: elements::issuance::AssetId,
-        value: u64,
-        script: BEScript,
-        confidential: bool,
-    ) -> Self {
-        UTXOInfo {
-            asset: asset.to_hex(),
-            value,
-            script,
-            confidential,
-        }
-    }
-
-    pub fn asset_id(&self) -> Option<elements::issuance::AssetId> {
-        if self.asset == "btc" {
-            None
-        } else {
-            Some(self.asset.parse().expect("Invalid asset"))
-        }
-    }
-}
 
 #[derive(Default)]
 pub struct ScriptBatch {
