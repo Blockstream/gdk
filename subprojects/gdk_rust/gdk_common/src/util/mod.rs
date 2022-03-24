e0e0f02c94a8e421aff4c0671e80fc57fa196c07
Leonardo Comandini <leo@leocomandini.org>
2022-03-24 18:14:33 +0100

    rust: fix is_confidential_txoutsecrets

--- subprojects/gdk_rust/gdk_common/src/util/mod.rs
+++ subprojects/gdk_rust/gdk_common/src/util/mod.rs
@@ -3,8 +3,8 @@ mod strser;
 pub use strser::StringSerialized;
 
 pub fn is_confidential_txoutsecrets(txoutsecrets: &elements::TxOutSecrets) -> bool {
-    txoutsecrets.asset_bf == elements::confidential::AssetBlindingFactor::zero()
-        && txoutsecrets.value_bf == elements::confidential::ValueBlindingFactor::zero()
+    txoutsecrets.asset_bf != elements::confidential::AssetBlindingFactor::zero()
+        && txoutsecrets.value_bf != elements::confidential::ValueBlindingFactor::zero()
 }
 
 pub fn weight_to_vsize(weight: usize) -> usize {
