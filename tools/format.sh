78d25ebc26d787a8172d002f3668487a136d7b1b
Jon Griffiths <jon_p_griffiths@yahoo.com>
2022-05-23 13:30:04 +1200

    tools: make rust formatting work outside of docker

--- tools/format.sh
+++ tools/format.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-files=$(echo src/*.{c,h}pp include/*.h tests/*cpp)
-files=$(echo $files | tr ' ' '\n' | grep -v generated | tr '\n' ' ')
-clang-format -i $files
+have_cmd()
+{
+    command -v "$1" >/dev/null 2>&1
+}
 
-if [ -f "/root/.cargo/env" ]; then
-    source /root/.cargo/env
-    pushd subprojects/gdk_rust
-    cargo fmt
-    popd
+if have_cmd clang-format; then
+    CLANG_FORMAT=$(command -v clang-format)
+    files=$(echo src/*.{c,h}pp include/*.h tests/*cpp)
+    files=$(echo $files | tr ' ' '\n' | grep -v generated | tr '\n' ' ')
+    $CLANG_FORMAT -i $files
+else
+    echo "WARNING: clang-format not found, C++ code not formatted"
+fi
+
+if ! have_cmd cargo; then
+    if [ -f "/root/.cargo/env" ]; then
+        # Docker CI
+        source /root/.cargo/env
+    fi
+fi
+
+if have_cmd cargo; then
+    CARGO=$(command -v cargo)
+    pushd subprojects/gdk_rust >/dev/null
+    $CARGO fmt
+    popd >/dev/null
+else
+    echo "WARNING: cargo not found, Rust code not formatted"
 fi
