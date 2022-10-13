ecae7c88034521ee5e7342209c7f7b14217046a5
gministeri <gministeri@blockstream.com>
2022-10-13 16:47:29 +0200

    [build-system] upgrading ubuntu 18.04 CI docker image
    Follow-up commit to update the Ubuntu CI docker image to exploit the new
    tools/builddeps.sh script and the new pracice of shipping pre-compiled
    dependencies within the docker images.

--- tools/ubuntu_18.04_deps.sh
+++ tools/ubuntu_18.04_deps.sh
@@ -4,7 +4,7 @@ set -e
 apt update -qq
 apt upgrade -yqq
 
-apt install curl autoconf pkg-config build-essential libtool virtualenv python3-{pip,yaml} ninja-build clang llvm-dev git swig unzip -yqq
+apt install curl autoconf pkg-config build-essential libtool virtualenv python3-{pip,yaml} ninja-build clang llvm-dev git swig unzip cmake -yqq
 pip3 install --require-hashes -r /requirements.txt
 rm /requirements.txt
 
