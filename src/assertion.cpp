28f77d010477ea2f2727889b9783ea7f4c0772f7
Jon Griffiths <jon_p_griffiths@yahoo.com>
2022-12-01 18:31:22 +1300

    Check assert condition before runtime_assert_message(), not inside it
    The condition is always evaluated in the callers context to pass to the
    function, along with all function arguments i.e. the message (which may
    be expensive to compute). Since the call is a no-op when the condition
    is true, avoid doing any work in this case.
    
    When debugging, a breakpoint on runtime_assert_message() can now be set
    instead of on the line inside it when trying to debug assertion failures.

--- src/assertion.cpp
+++ src/assertion.cpp
@@ -6,19 +6,16 @@
 
 namespace ga {
 namespace sdk {
-    void runtime_assert_message(
-        bool condition, const std::string& error_message, const char* file, const char* func, const char* line)
+    void runtime_assert_message(const std::string& error_message, const char* file, const char* func, const char* line)
     {
-        if (!condition) {
-            const std::string msg
-                = std::string("assertion failure: ") + file + ":" + func + ":" + line + ":" + error_message;
-            GDK_LOG_SEV(log_level::error) << msg
+        const std::string msg
+            = std::string("assertion failure: ") + file + ":" + func + ":" + line + ":" + error_message;
+        GDK_LOG_SEV(log_level::error) << msg
 #if defined(__linux__) and not defined(NDEBUG) and defined(HAVE_BACKTRACE)
-                                          << "\n:backtrace " << boost::stacktrace::stacktrace()
+                                      << "\n:backtrace " << boost::stacktrace::stacktrace()
 #endif
-                ;
-            throw assertion_error(msg);
-        }
+            ;
+        throw assertion_error(msg);
     }
 } // namespace sdk
 } // namespace ga
--- src/assertion.hpp
+++ src/assertion.hpp
@@ -6,19 +6,26 @@
 
 namespace ga {
 namespace sdk {
-    void runtime_assert_message(
-        bool condition, const std::string& error_message, const char* file, const char* func, const char* line);
+    void runtime_assert_message(const std::string& error_message, const char* file, const char* func, const char* line);
 }
 } // namespace ga
 
 #define GDK_STRINGIFY_(x) #x
 #define GDK_STRINGIFY(x) GDK_STRINGIFY_(x)
 #define GDK_RUNTIME_ASSERT(condition)                                                                                  \
-    ga::sdk::runtime_assert_message(                                                                                   \
-        condition, std::string(), __FILE__, static_cast<const char*>(__func__), GDK_STRINGIFY(__LINE__))
+    do {                                                                                                               \
+        if (!(condition)) {                                                                                            \
+            ga::sdk::runtime_assert_message(                                                                           \
+                std::string(), __FILE__, static_cast<const char*>(__func__), GDK_STRINGIFY(__LINE__));                 \
+        }                                                                                                              \
+    } while (false)
 #define GDK_RUNTIME_ASSERT_MSG(condition, error_message)                                                               \
-    ga::sdk::runtime_assert_message(                                                                                   \
-        condition, error_message, __FILE__, static_cast<const char*>(__func__), GDK_STRINGIFY(__LINE__))
+    do {                                                                                                               \
+        if (!(condition)) {                                                                                            \
+            ga::sdk::runtime_assert_message(                                                                           \
+                error_message, __FILE__, static_cast<const char*>(__func__), GDK_STRINGIFY(__LINE__));                 \
+        }                                                                                                              \
+    } while (false)
 #define GDK_VERIFY(x) GDK_RUNTIME_ASSERT((x) == WALLY_OK)
 
 #define NET_ERROR_CODE_CHECK(msg, ec)                                                                                  \
