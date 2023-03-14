cb9f938fc104379313f916a840af8838bd5b372e
Jon Griffiths <jon_p_griffiths@yahoo.com>
2023-03-14 23:02:16 +1300

    build: update fedora build as per ubuntu

--- tools/fedora32_deps.sh
+++ tools/fedora32_deps.sh
@@ -3,11 +3,11 @@ set -e
 
 dnf update -yq
 dnf install -yq @development-tools autoconf pkg-config libtool ninja-build clang which python python3-pip libatomic curl perl-core cmake libstdc++-static
-pip install --require-hashes -r /requirements.txt
+
+pip install --require-hashes -r tools/requirements.txt
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.64.0
 source /root/.cargo/env
-rustup component add rustfmt clippy
 
 mkdir /tmp/protoc && \
     cd /tmp/protoc && \
