a808ab50de676c0cd4d78ffc8d23d6f0d2619442
Jon Griffiths <jon_p_griffiths@yahoo.com>
2023-05-24 18:46:22 +1200

    logging: don't stringify line numbers, pass them as integers for logging
    Avoids excess string constants and relocations in the final library.

--- src/assertion.cpp
+++ src/assertion.cpp
@@ -6,10 +6,10 @@
 
 namespace ga {
 namespace sdk {
-    void runtime_assert_message(const std::string& error_message, const char* file, const char* func, const char* line)
+    void runtime_assert_message(const std::string& error_message, const char* file, const char* func, unsigned int line)
     {
         const std::string msg
-            = std::string("assertion failure: ") + file + ":" + func + ":" + line + ":" + error_message;
+            = std::string("assertion failure: ") + file + ":" + func + ":" + std::to_string(line) + ":" + error_message;
         GDK_LOG_SEV(log_level::error) << msg
 #if defined(__linux__) and not defined(NDEBUG) and defined(HAVE_BACKTRACE)
                                       << "\n:backtrace " << boost::stacktrace::stacktrace()
--- src/assertion.hpp
+++ src/assertion.hpp
@@ -6,24 +6,21 @@
 
 namespace ga {
 namespace sdk {
-    void runtime_assert_message(const std::string& error_message, const char* file, const char* func, const char* line);
+    void runtime_assert_message(
+        const std::string& error_message, const char* file, const char* func, unsigned int line);
 }
 } // namespace ga
 
-#define GDK_STRINGIFY_(x) #x
-#define GDK_STRINGIFY(x) GDK_STRINGIFY_(x)
 #define GDK_RUNTIME_ASSERT(condition)                                                                                  \
     do {                                                                                                               \
         if (!(condition)) {                                                                                            \
-            ga::sdk::runtime_assert_message(                                                                           \
-                std::string(), __FILE__, static_cast<const char*>(__func__), GDK_STRINGIFY(__LINE__));                 \
+            ga::sdk::runtime_assert_message(std::string(), __FILE__, static_cast<const char*>(__func__), __LINE__);    \
         }                                                                                                              \
     } while (false)
 #define GDK_RUNTIME_ASSERT_MSG(condition, error_message)                                                               \
     do {                                                                                                               \
         if (!(condition)) {                                                                                            \
-            ga::sdk::runtime_assert_message(                                                                           \
-                error_message, __FILE__, static_cast<const char*>(__func__), GDK_STRINGIFY(__LINE__));                 \
+            ga::sdk::runtime_assert_message(error_message, __FILE__, static_cast<const char*>(__func__), __LINE__);    \
         }                                                                                                              \
     } while (false)
 #define GDK_VERIFY(x) GDK_RUNTIME_ASSERT((x) == WALLY_OK)
