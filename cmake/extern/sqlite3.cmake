5abb1708c71e533972e3a28045c4215004144913
gministeri <gministeri@blockstream.com>
2023-05-29 11:04:09 +0200

    build-system: python-wheel build no longer depends on full target
    With the intent of removing the greenaddress-full target, this commit
    changes the dependency declaration of python-wheel taget.
    
    setup.py files now list all the required dependencies

--- cmake/extern/sqlite3.cmake
+++ cmake/extern/sqlite3.cmake
@@ -1,7 +1,10 @@
 #sqlite
 join_path(_sqlitePkgDir ${EXTERNAL-DEPS-DIR} "sqlite" "build")
+join_path(sqlite3_LINK_LIBRARIES ${_sqlitePkgDir} "lib" "libsqlite3.a")
+join_path(sqlite3_INCLUDE_DIRECTORIES ${_sqlitePkgDir} "include")
+
 add_library(sqlite3 STATIC IMPORTED)
 set_target_properties(sqlite3 PROPERTIES
-    IMPORTED_LOCATION ${_sqlitePkgDir}/lib/libsqlite3.a
-    INTERFACE_INCLUDE_DIRECTORIES ${_sqlitePkgDir}/include
+    IMPORTED_LOCATION ${sqlite3_LINK_LIBRARIES}
+    INTERFACE_INCLUDE_DIRECTORIES ${sqlite3_INCLUDE_DIRECTORIES}
 )
