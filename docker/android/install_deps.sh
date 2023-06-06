d267681e0d258e665de1d4045c100aa185a949cb
gministeri <gministeri@blockstream.com>
2023-06-06 09:06:49 +0200

    build-system: adding android docker image
    New docker image dedicated to build gdk for android platform.
    Debian image gets lighter

--- docker/android/install_deps.sh
+++ docker/android/install_deps.sh
@@ -4,8 +4,13 @@ set -e
 apt update -qq
 apt upgrade -yqq
 
-apt install --no-install-recommends autoconf automake autotools-dev pkg-config build-essential libtool python3{,-dev,-pip,-virtualenv} python{,-dev}-is-python3 ninja-build clang{,-format,-tidy} git swig g++-mingw-w64-x86-64 curl cmake libssl-dev libtool-bin patchelf -yqq
-pip3 install --require-hashes -r tools/requirements.txt
+apt install --no-install-recommends unzip autoconf automake autotools-dev pkg-config build-essential libtool python3{,-dev} python{,-dev}-is-python3 clang git swig openjdk-11-jdk curl cmake libssl-dev libtool-bin -yqq
+update-java-alternatives -s java-1.11.0-openjdk-amd64
+
+curl -L -o ndk.zip https://dl.google.com/android/repository/android-ndk-r23b-linux.zip
+echo "c6e97f9c8cfe5b7be0a9e6c15af8e7a179475b7ded23e2d1c1fa0945d6fb4382 ndk.zip" | sha256sum --check
+unzip ndk.zip
+rm ndk.zip
 
 
 mkdir /tmp/protoc && \
