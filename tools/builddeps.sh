547f318def0297318bc2c9b89544537528d47fbc
gministeri <gministeri@blockstream.com>
2023-07-26 12:19:37 +0200

    builddeps: restoring json version in tar file

--- tools/builddeps.sh
+++ tools/builddeps.sh
@@ -208,7 +208,7 @@ function prepare_sources {
     fi
     if [ ! -f ${source_filename} ]; then
         echo "downloading from ${source_url} ..."
-        curl -sL --retry 3 -o ${source_filename} ${source_url}
+        curl -sL --retry 3 -o ${source_filename} ${source_url} --output ${source_filename}
         echo "${source_hash}  ${source_filename}" | shasum -a 256 -c
         rm_downloaded="yes"
     fi
@@ -335,7 +335,7 @@ build ${name} ${source_name} "tmp"
 name="nlohmann_json"
 source_url="https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz"
 source_name="json"
-source_filename="${source_name}.tar.xz"
+source_filename="json-3.11.2.tar.xz"
 source_hash="8c4b26bf4b422252e13f332bc5e388ec0ab5c3443d24399acb675e68278d341f"
 prepare_sources ${source_url} ${source_filename} ${source_hash}
 cmake -B tmp/${source_name}/build -S tmp/${source_name} \
