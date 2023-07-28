b107f2242d889864a9a1f790a15a4354c2764032
Leonardo Comandini <leo@leocomandini.org>
2023-07-28 13:14:15 +0200

    rust: remove sighashtype

--- subprojects/gdk_rust/gdk_common/src/be/mod.rs
+++ subprojects/gdk_rust/gdk_common/src/be/mod.rs
@@ -3,7 +3,6 @@ mod blockhash;
 mod blockheader;
 mod outpoint;
 mod script;
-mod sighashtype;
 mod transaction;
 mod txid;
 
@@ -13,7 +12,6 @@ pub use blockhash::*;
 pub use blockheader::*;
 pub use outpoint::*;
 pub use script::*;
-pub use sighashtype::*;
 use std::fmt::Debug;
 pub use transaction::*;
 pub use txid::*;
