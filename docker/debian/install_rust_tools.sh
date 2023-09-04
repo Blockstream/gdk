52dedb27120dcf19ddb0dcba3a4fe5c23180163f
gministeri <gministeri@blockstream.com>
2023-09-04 10:07:05 +0200

    pinning versions of rust tools in debian docker image

---
+++ docker/debian/install_rust_tools.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+set -e
+
+rustup component add rustfmt clippy llvm-tools-preview
+rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android x86_64-pc-windows-gnu
+cargo install --locked --version 0.17.6 cargo-audit
+cargo install --locked --version 0.8.19 grcov
+cargo install --locked --version 0.9.55 cargo-nextest
