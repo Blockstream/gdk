d10897e98843a1714020eb21c82d26246b9ed1b5
gministeri <gministeri@blockstream.com>
2023-10-27 15:30:40 +0200

    adding java binding export to debian
    Clients find themselves in need to build gdk+java bindings
    for docker environment or native debian.
    It already happened twice: time to automate the process.

--- docker/debian/install_deps.sh
+++ docker/debian/install_deps.sh
@@ -3,7 +3,7 @@ set -e
 
 apt update -qq
 
-apt install --no-install-recommends autoconf automake autotools-dev pkg-config build-essential libtool python3{,-dev,-pip,-virtualenv} python{,-dev}-is-python3 clang{,-format,-tidy} git swig g++-mingw-w64-x86-64 curl cmake libssl-dev libtool-bin -yqq
+apt install --no-install-recommends autoconf automake autotools-dev pkg-config build-essential libtool python3{,-dev,-pip,-virtualenv} python{,-dev}-is-python3 clang{,-format,-tidy} git swig g++-mingw-w64-x86-64 curl cmake libssl-dev libtool-bin openjdk-11-jdk openjdk-11-jre -yqq
 pip3 install --require-hashes -r tools/requirements.txt
 
 
