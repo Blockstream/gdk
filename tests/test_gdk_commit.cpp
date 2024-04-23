b14e8d3f770f45547f009682e50fa3d9fdf6feed
gministeri <gministeri@blockstream.com>
2024-04-23 09:58:42 +0200

    cmake: fix GDK_COMMIT string in version.h

---
+++ tests/test_gdk_commit.cpp
@@ -0,0 +1,16 @@
+#include "assertion.hpp"
+#include "version.h"
+
+#include <algorithm>
+#include <functional>
+#include <locale>
+#include <string>
+
+int main()
+{
+    std::string sha = GDK_COMMIT;
+    GDK_RUNTIME_ASSERT(sha.length() == 40);
+    std::locale loc;
+    std::function<bool(char)> isalnum = std::bind(std::isalnum<char>, std::placeholders::_1, loc);
+    GDK_RUNTIME_ASSERT(std::all_of(sha.cbegin(), sha.cend(), isalnum));
+}
