d82ce2b0b2394f0b149a60e65036dcc3428597e2
gministeri <gministeri@blockstream.com>
2024-07-23 10:39:32 +0200

    ur-c: remove bc-ur format functions

--- src/bcur_auth_handlers.cpp
+++ src/bcur_auth_handlers.cpp
@@ -28,135 +28,18 @@ namespace green {
 
 #ifdef USE_REAL_BCUR
     namespace {
-        // FIXME: this whole set of functions inside this anonymous namespace should be moved to ur-c library
 
-        static std::string format_eckey(const crypto_eckey& key)
-        {
-            switch (key.type) {
-            case crypto_eckey::eckey_type_private:
-                return b2h(key.key.prvate);
-            case crypto_eckey::eckey_type_public_compressed:
-                return b2h(key.key.public_compressed);
-            case crypto_eckey::eckey_type_public_uncompressed:
-                return b2h(key.key.public_uncompressed);
-            default:
-                throw user_error("unhandled eckey type");
-            }
-        }
-
-        static std::string format_hdkey(const crypto_hdkey& key, bool is_bip44 = false)
-        {
-            std::string keyorigin;
-            keyorigin.resize(15);
-            int len = static_cast<int>(keyorigin.size());
-            while (len >= static_cast<int>(keyorigin.size())) {
-                keyorigin.resize(keyorigin.size() * 2);
-                len = format_keyorigin(&key, keyorigin.data(), keyorigin.size());
-                if (len < 0) {
-                    throw user_error("crypto_hdkey not available");
-                }
-            }
-            keyorigin.resize(len);
-
-            std::array<uint8_t, BIP32_SERIALIZED_LEN> bip32 = { 0 }; // Initialize to prevent clang-tidy warning
-            if (!bip32_serialize(&key, bip32.data())) {
-                throw user_error("hdkey2bip32 failure");
-            }
-            std::string derivationpath;
-            derivationpath.resize(5);
-            len = static_cast<int>(derivationpath.size());
-            while (len >= static_cast<int>(derivationpath.size())) {
-                derivationpath.resize(derivationpath.capacity() * 2);
-                len = format_keyderivationpath(&key, derivationpath.data(), derivationpath.capacity());
-                if (len < 0) {
-                    throw user_error("crypto_hdkey not available");
-                }
-            }
-
-            derivationpath.resize(len);
-            if (is_bip44 && derivationpath.empty()) {
-                /* HD Keys inside crypto-account do not include the
-                 * derivation path, but are only allowed to be bip44
-                 * compatible. Append the bip44 derivation so
-                 * callers can use the resulting descriptor.
-                 * TODO: The correct path is "/<0;1>/<star>", but wallets
-                 * that do not understand multi-index descriptors require
-                 * "/0/<star>" and must infer the change addresses themselves.
-                 */
-                derivationpath = "/0/*";
-            }
-            return keyorigin + base58check_from_bytes(bip32) + derivationpath;
-        }
-
-        static std::string format_output(const crypto_output& output, bool is_bip44 = false)
-        {
-            std::string descriptor;
-            std::string descriptor_end;
-            switch (output.type) {
-            case crypto_output::output_type__:
-                break;
-            case crypto_output::output_type_sh:
-                descriptor = "sh(";
-                descriptor_end = ")";
-                break;
-            case crypto_output::output_type_wsh:
-                descriptor += "wsh(";
-                descriptor_end += ")";
-                break;
-            case crypto_output::output_type_sh_wsh:
-                descriptor += "sh(wsh(";
-                descriptor_end += "))";
-                break;
-            case crypto_output::output_type_rawscript:
-                return "raw(" + b2h(output.output.raw) + ")";
-            default:
-                throw user_error("crypto_output not available");
-            }
-            switch (output.output.key.type) {
-            case output_keyexp::keyexp_type_pk:
-                descriptor += "pk(";
-                descriptor_end += ")";
-                break;
-            case output_keyexp::keyexp_type_pkh:
-                descriptor += "pkh(";
-                descriptor_end += ")";
-                break;
-            case output_keyexp::keyexp_type_wpkh:
-                descriptor += "wpkh(";
-                descriptor_end += ")";
-                break;
-            case output_keyexp::keyexp_type_cosigner:
-                /* NOTE: cosigner() is just made-up nonsense, no software will
-                 *       decode it and no BIP has ever been proposed for it.
-                 */
-                descriptor += "cosigner(";
-                descriptor_end += ")";
-                break;
-            case output_keyexp::keyexp_type_na:
-                throw user_error("output_keyexp not available");
-            }
-            const auto& key = output.output.key;
-            switch (key.keytype) {
-            case output_keyexp::keyexp_keytype_eckey:
-                /* EC Keys cannot appear in 'crypto-account's so no bip44
-                 * path needs to be appended.
-                 */
-                return descriptor + format_eckey(key.key.eckey) + descriptor_end;
-                break;
-            case output_keyexp::keyexp_keytype_hdkey:
-                return descriptor + format_hdkey(key.key.hdkey, is_bip44) + descriptor_end;
-                break;
-            case output_keyexp::keyexp_keytype_na:
-                throw user_error("output_keyexp not available");
-            }
-            GDK_RUNTIME_ASSERT(false);
-            __builtin_unreachable();
-        }
+        using urc_string_ptr = std::unique_ptr<char, decltype(&urc_string_free)>;
+        using urc_buffer_ptr = std::unique_ptr<uint8_t, decltype(&urc_free)>;
+        using urc_psbt_ptr = std::unique_ptr<crypto_psbt, decltype(&urc_crypto_psbt_free)>;
+        using urc_string_array_ptr = std::unique_ptr<char*[], decltype(&urc_string_array_free)>;
+        using urc_jade_bip8539_response_ptr
+            = std::unique_ptr<jade_bip8539_response, decltype(&urc_jade_bip8539_response_free)>;
 
         static nlohmann::json deserialize_psbt(const std::vector<uint8_t>& cbor)
         {
             crypto_psbt psbt;
-            std::unique_ptr<crypto_psbt, decltype(&urc_crypto_psbt_free)> holder(&psbt, urc_crypto_psbt_free);
+            urc_psbt_ptr holder(&psbt, urc_crypto_psbt_free);
             int result = urc_crypto_psbt_deserialize(cbor.data(), cbor.size(), &psbt);
             if (result != URC_OK) {
                 throw user_error("ur-c: Parsing crypto_psbt failed with error code:" + std::to_string(result));
@@ -170,7 +53,14 @@ namespace green {
             int result = urc_crypto_output_deserialize(raw.data(), raw.size(), &output);
             GDK_RUNTIME_ASSERT_MSG(
                 result == URC_OK, "ur-c: Parsing crypto-output failed with error code:" + std::to_string(result));
-            return { { "descriptor", format_output(output) } };
+            char* formatted_output = nullptr;
+            urc_string_ptr holder(formatted_output, urc_string_free);
+            // Add missing "[0;1]/*" derivation elements to BIP44 paths
+            const auto mode = urc_crypto_output_format_mode_BIP44_compatible;
+            result = urc_crypto_output_format(&output, mode, &formatted_output);
+            GDK_RUNTIME_ASSERT_MSG(
+                result == URC_OK, "ur-c: crypto-output format failed with error code:" + std::to_string(result));
+            return { { "descriptor", formatted_output } };
         }
 
         static nlohmann::json deserialize_account(const std::vector<uint8_t>& raw)
@@ -182,10 +72,15 @@ namespace green {
                 GDK_RUNTIME_ASSERT_MSG(
                     result == URC_OK, "ur-c: Parsing account failed with error code:" + std::to_string(result));
             }
+
+            char** formatted_descriptors = nullptr;
+            urc_string_array_ptr holder(formatted_descriptors, urc_string_array_free);
+            result = urc_crypto_account_format(
+                &account, urc_crypto_output_format_mode_BIP44_compatible, &formatted_descriptors);
             nlohmann::json::array_t descriptors;
-            for (size_t i = 0; i < account.descriptors_count; i++) {
-                constexpr bool is_bip44 = true;
-                descriptors.push_back(format_output(account.descriptors[i], is_bip44));
+            size_t i = 0;
+            while (formatted_descriptors[i] != nullptr) {
+                descriptors.push_back(formatted_descriptors[i++]);
             }
             auto fingerprint = (boost::format("%08x") % account.master_fingerprint).str();
             return { { "master_fingerprint", fingerprint }, { "descriptors", std::move(descriptors) } };
@@ -195,6 +90,7 @@ namespace green {
             const std::vector<uint8_t>& raw, const std::optional<std::string>& private_key)
         {
             jade_bip8539_response response;
+            urc_jade_bip8539_response_ptr holder(&response, urc_jade_bip8539_response_free);
             int result = urc_jade_bip8539_response_deserialize(raw.data(), raw.size(), &response);
             GDK_RUNTIME_ASSERT_MSG(result == URC_OK, "internal ur-c error, error_code: " + std::to_string(result));
             if (!private_key) {
@@ -202,7 +98,6 @@ namespace green {
                     { "public_key", b2h({ response.pubkey, EC_PUBLIC_KEY_LEN }) },
                     { "encrypted", b2h({ response.encrypted_data, response.encrypted_len }) },
                 };
-                urc_jade_bip8539_response_free(&response);
                 return retv;
             }
             std::vector<uint8_t> privkey = h2b(private_key.value());
@@ -216,7 +111,6 @@ namespace green {
                 salt.size(), AES_FLAG_DECRYPT, &buffer_required_len);
 
             if (wally_error != WALLY_OK) {
-                urc_jade_bip8539_response_free(&response);
                 throw user_error(
                     "internal wally error on parse_jaderesponse, error_code: " + std::to_string(wally_error));
             }
@@ -227,7 +121,6 @@ namespace green {
             wally_error = wally_aes_cbc_with_ecdh_key(privkey.data(), privkey.size(), nullptr, 0,
                 response.encrypted_data, response.encrypted_len, response.pubkey, EC_PUBLIC_KEY_LEN, salt.data(),
                 salt.size(), AES_FLAG_DECRYPT, jade_entropy.data(), jade_entropy.size(), &buffer_written_len);
-            urc_jade_bip8539_response_free(&response);
             GDK_RUNTIME_ASSERT_MSG(wally_error == WALLY_OK,
                 "internal wally error on parse_jaderesponse, error_code: " + std::to_string(wally_error));
             jade_entropy.resize(buffer_written_len);
@@ -241,8 +134,8 @@ namespace green {
         nlohmann::json deserialize_jaderpc(const green::byte_span_t raw)
         {
             char* raw_json = nullptr;
+            urc_string_ptr holder(raw_json, urc_string_free);
             int result = urc_jade_rpc_deserialize(raw.data(), raw.size(), &raw_json);
-            std::unique_ptr<char, decltype(&urc_string_free)> holder(raw_json, urc_string_free);
             GDK_RUNTIME_ASSERT_MSG(result == URC_OK, "internal ur-c error, error_code: " + std::to_string(result));
             return json_parse(raw_json);
         }
@@ -277,8 +170,8 @@ namespace green {
 
             uint8_t* buffer = nullptr;
             size_t buffer_len = 0;
+            urc_buffer_ptr holder(buffer, urc_free);
             int result = urc_jade_bip8539_request_serialize(&request, &buffer, &buffer_len);
-            std::unique_ptr<uint8_t, decltype(&urc_free)> holder(buffer, urc_free);
             GDK_RUNTIME_ASSERT_MSG(result == URC_OK, "internal ur-c error, error_code: " + std::to_string(result));
             ur::UR ur("jade-bip8539-request", { buffer, buffer + buffer_len });
             return ur;
@@ -289,8 +182,8 @@ namespace green {
             std::vector<uint8_t> raw = base64_to_bytes(base64_psbt);
             const crypto_psbt psbt{ raw.data(), raw.size() };
             uint8_t* cbor = nullptr;
-            std::unique_ptr<uint8_t, decltype(&urc_free)> hojder(cbor, urc_free);
             size_t cbor_len = 0;
+            urc_buffer_ptr holder(cbor, urc_free);
             int result = urc_crypto_psbt_serialize(&psbt, &cbor, &cbor_len);
             if (result != URC_OK) {
                 throw user_error("ur-c: Serializing crypto_psbt failed with error code:" + std::to_string(result));
