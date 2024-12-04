a0adfc1e6fd514e970ca3ee1a1c5759858c6a7c3
Jon Griffiths <jon_p_griffiths@yahoo.com>
2024-12-04 20:37:36 +1300

    build: add a local Dockerfile for building AWS compatible gdk versions

---
+++ docker/aws_lambda/install_deps.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+set -e
+
+yum update -y -q
+yum install -y -q @development-tools autoconf pkgconfig libtool clang which python python3-pip libatomic curl perl-core libstdc++-static xz gzip git make patch swig
+
+# We need a more recent cmake than the one in the distro repos
+curl -sL --retry 3 https://github.com/Kitware/CMake/releases/download/v3.18.5/cmake-3.18.5-Linux-x86_64.sh --output cmake.sh
+chmod +x cmake.sh
+./cmake.sh --skip-license --exclude-subdir --prefix=/usr/local/
+
+pip install --require-hashes -r tools/requirements.txt
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.68.0
+source /root/.cargo/env
