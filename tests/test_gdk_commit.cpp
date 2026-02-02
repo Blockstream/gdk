59ee89fd3858e54a1cee362421b839ae792475d5
Jon Griffiths <jon_p_griffiths@yahoo.com>
2026-02-02 14:46:16 +0100

    login: use the short git sha in the login user agent

--- tests/test_gdk_commit.cpp
+++ tests/test_gdk_commit.cpp
@@ -9,7 +9,7 @@
 int main()
 {
     std::string sha = GDK_COMMIT;
-    GDK_RUNTIME_ASSERT(sha.length() == 40);
+    GDK_RUNTIME_ASSERT(sha.length() == 8);
     std::locale loc;
     std::function<bool(char)> isalnum = std::bind(std::isalnum<char>, std::placeholders::_1, loc);
     GDK_RUNTIME_ASSERT(std::all_of(sha.cbegin(), sha.cend(), isalnum));
